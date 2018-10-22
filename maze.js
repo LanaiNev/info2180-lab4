8c87ed7546782ccd3a4a09acdf328467272ca2f8
Student058 <student058@Eng-01.uwimona.edu.jm>
2018-10-22 17:33:55 -0500

    Exercise 6

--- maze.js
+++ maze.js
@@ -2,12 +2,15 @@ var mazebound;
 var start;
 var end;
 var state;
+var leave;
 var checkstart = false;
 var win = true;
+
 
 window.onload = function () {
 	mazebound = document.querySelectorAll(".boundary"); //Collects all .boundary CSS Selectors
 	state = document.getElementById("status");			//Collects all HTML status
+	leave = document.getElementById("maze");
 	
 	
 	start = document.getElementById("start");			//Collects all HTML start
@@ -21,7 +24,9 @@ window.onload = function () {
 	start.onmouseover = Starter;						//Checks if user hovered over the Start Button
 	start.onclick = newGame;							//Click to reset the game
 	end.onmouseover = WinCheck;							//Checks if user hovered over the End Button
+	leave.onmouseleave = Cheat;							//Checks if user has exited the maze and attempts to cheat
 }
+
 
 function mouseOver() {
 	win = false;
@@ -32,10 +37,12 @@ function mouseOver() {
 	}	
 }
 
+
 function Starter(){
 	checkstart = true;
 	return checkstart;
 }
+
 
 function WinCheck(){
 	if (win){
@@ -45,11 +52,24 @@ function WinCheck(){
 	}
 }
 
+
 function newGame(){
 	win = true;
-	state.innerText = 'New Game :)';			
+	state.innerText = 'New Game :)';	
+			
 	for (var i = 0; i < mazebound.length - 1; i++) {
 		mazebound[i].className = "boundary";
 	}
 }
 
+
+function Cheat(){
+	if (checkstart){
+		if (leave.onmouseleave){
+			state.innerText = 'Cheat :)		Click "S" to restart.';	
+			for (var i = 0; i < mazebound.length - 1; i++) {
+				mazebound[i].className += " youlose";			//Changes wall colour
+			}		
+		}
+	}
+}
