b952593c7c3fd6000e2f82b8d2328dfd19e34ae3
Student020 <student020@Eng-01.uwimona.edu.jm>
2018-10-22 16:04:56 -0500

    Exercise 3

--- maze.js
+++ maze.js
@@ -1,17 +1,41 @@
 var mazebound;
+var start;
+var end;
+var checkstart = false;
+var win = true;
 
 window.onload = function () {
 	mazebound = document.querySelectorAll(".boundary"); //Collects all .boundary CSS Selectors
 	
+	start = document.getElementById("start");
+	checkstart = false;
+	end = document.getElementById("end");
+
 	for (var i = 0; i < mazebound.length - 1; i++) {
 		mazebound[i].onmouseover = mouseOver;
 	}
 	
+	start.onmouseover = Starter;
+	end.onmouseover = WinCheck;
 }
 
-function mouseOver() {
+function mouseOver() {	
+	win = false;
 	for (var i = 0; i < mazebound.length - 1; i++) {
 		mazebound[i].className += " youlose";
 	}	
+}
+
+function Starter(){
+	checkstart = true;
+	return checkstart;
+}
+
+function WinCheck(){
+	if (win){
+		if(checkstart && mouseOver) { 
+			alert ('You Win :D');	
+		}
+	}
 }
 
